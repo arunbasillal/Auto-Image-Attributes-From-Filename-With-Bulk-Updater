5b5f62ab6193a96001f38cfa58f9fe120491e668
Arun Basil Lal <arunbasillal@gmail.com>
2023-08-07 14:18:11 +0530

    Display image attributes as columns in the Media Library.

--- loader.php
+++ loader.php
@@ -5,18 +5,18 @@
  * @since	1.3
  */
 
-
 // Exit if accessed directly
-if ( ! defined('ABSPATH') ) exit;
+if ( ! defined( 'ABSPATH' ) ) exit;
 
 // Load basic setup. Plugin list links, text domain, footer links etc. 
-require_once( IAFF_IMAGE_ATTRIBUTES_FROM_FILENAME_DIR . '/admin/basic-setup.php');
+require_once( IAFF_IMAGE_ATTRIBUTES_FROM_FILENAME_DIR . '/admin/basic-setup.php' );
 
 // Load admin setup. Register menus and settings
-require_once( IAFF_IMAGE_ATTRIBUTES_FROM_FILENAME_DIR . '/admin/admin-setup.php');
+require_once( IAFF_IMAGE_ATTRIBUTES_FROM_FILENAME_DIR . '/admin/admin-setup.php' );
 
 // Render Admin UI
-require_once( IAFF_IMAGE_ATTRIBUTES_FROM_FILENAME_DIR . '/admin/admin-ui-render.php');
+require_once( IAFF_IMAGE_ATTRIBUTES_FROM_FILENAME_DIR . '/admin/admin-ui-render.php' );
+require_once( IAFF_IMAGE_ATTRIBUTES_FROM_FILENAME_DIR . '/admin/columns-media-library.php' );
 
 // Do plugin operations
-require_once( IAFF_IMAGE_ATTRIBUTES_FROM_FILENAME_DIR . '/admin/do.php');
+require_once( IAFF_IMAGE_ATTRIBUTES_FROM_FILENAME_DIR . '/admin/do.php' );
